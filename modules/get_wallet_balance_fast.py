de6bd0beec2efc4232ea07be965153cd34331641
Denis Humen <dberkut18@gmail.com>
2025-03-17 15:18:09 +0200

    починил fast режим, все еще нужно править и модифицировать, но уже можно пользоваться

--- modules/get_wallet_balance_fast.py
+++ modules/get_wallet_balance_fast.py
@@ -12,7 +12,7 @@ def get_wallet_balance_fast(address, rpc_url, proxies):
             web3 = Web3(Web3.HTTPProvider(rpc_url, request_kwargs={'proxies': proxies_dict}))
             checksum_address = web3.to_checksum_address(address)
             balance = web3.eth.get_balance(checksum_address)
-            return web3.fromWei(balance, 'ether')
+            return web3.from_wei(balance, 'ether')
         except Exception as e:
             print(f"Error with proxy {proxy}: {e}")
             proxies.remove(proxy)
